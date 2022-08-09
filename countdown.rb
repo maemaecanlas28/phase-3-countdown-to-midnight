ac16e6b03532a8cb245e21aa2c0fedbf06b60e6f
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-08 21:26:36 -0700

    commit tree 63b2e0f509bd27c9e3de0f15dbbc93f288570d81

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+i = 10
+def countdown (num)
+    while num >= 1
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+# countdown(i);
+
+def countdown_with_sleep (num)
+    while num >= 1
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        sleep(1) 
+    end
+    "HAPPY NEW YEAR!"
+end
+
+# countdown_with_sleep(i)
